69f28bc48269693b2031fecf52fa7e78c984ca1c
Rene Demir geborener Enstone <89888045+KinGKrAss@users.noreply.github.com>
2026-08-25 12:00:24 +0200

    feat(knowledge): initialize Z1 knowledge package

---
+++ z1/knowledge/__init__.py
@@ -0,0 +1,6 @@
+"""Z1 knowledge and memory package."""
+
+from .meta_wiki import KnowledgeEntry, MetaWiki, VerificationStatus
+from .system_memory import SystemZ1Memory
+
+__all__ = ["KnowledgeEntry", "MetaWiki", "SystemZ1Memory", "VerificationStatus"]
